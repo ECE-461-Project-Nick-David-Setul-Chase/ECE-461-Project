bd64108da15d00094262ce10fc071456c76aaf59
NickW09 <nickcreo@yahoo.com>
2023-02-05 17:25:54 -0500

    Update GraphQL.py

--- API/GraphQL.py
+++ API/GraphQL.py
@@ -2,13 +2,27 @@ import requests
 import json
 
 #Input: url = str, api_token = str
-#Output: data = str
-def call_graphQL(url, api_token):
-    #url = 'https://api.github.com/graphql'
-    #api_token = "PUT TOKEN HERE"
-    json = { 'query' : '{ viewer { repositories(first: 30) { totalCount pageInfo { hasNextPage endCursor } edges { node { name } } } } }' }
-    headers = {'Authorization': 'token %s' % api_token}
-    r = requests.post(url=url, json=json, headers=headers)
-    print (r.text)
+#Output: data = dictionary
+def call_graphQL(url_, api_token):
 
-    return r
+    #Defines what we want
+    json_ = { 'query' : '{ viewer { repositories(first: 30) { totalCount pageInfo { hasNextPage endCursor } edges { node { name } } } } }' }
+    #Data sent to API
+    headers = {'Authorization': 'token %s' % api_token}
+    #Collecting data
+    data_dict = requests.post(url=url_, json=json_, headers=headers)
+
+    return data_dict.text
+
+
+#Number of comments
+#Does README exist?
+#Does documentation exist?
+#Number of contributors
+#Number of issues
+#GNU LGPL 2.1 compatible/check licensing
+
+def idk():
+    response = requests.get(url)
+    if(response != 200):
+        print("GraphQL.py: Query Request Error Code {response}")
