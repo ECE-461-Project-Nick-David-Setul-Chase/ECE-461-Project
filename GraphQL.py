8420dbdb65022b1423601ef261fb8e6b26185bfc
NickW09 <60638129+NickW09@users.noreply.github.com>
2023-02-08 14:45:00 -0500

    Update GraphQL.py

--- GraphQL.py
+++ GraphQL.py
@@ -5,14 +5,15 @@ import requests
 #Returns: Dictionary starting with 'data' key
 def call_graphQL(url_, api_token):
 
+    url_graphQL = 'https://api.github.com/graphql'
+    
     #URL format: https://github.com/user/repo
 
     #Grabbing everything past https://github.com/
     url_useful = url_[19:]
 
     #Find index of start of repo name
-    repo_index = url_.find('/')
-
+    repo_index = url_useful.find('/')
     user = url_useful[0 : repo_index]
     repo = url_useful[(repo_index + 1) :]
 
@@ -20,8 +21,16 @@ def call_graphQL(url_, api_token):
     query_ = '''
     { 
         repository(owner:"''' + user + '''", name:"''' + repo + '''") {
-            issues(states:OPEN) {
+            issuesOpen: issues(states:OPEN){
                 totalCount
+            }
+            issuesClosed: issues(states:CLOSED){
+                totalCount
+            }
+            issueLastOpened: issues(states:OPEN, orderBy:{field: CREATED_AT, direction: DESC}, first:1){
+                nodes{
+                    createdAt
+                }
             }
             assignableUsers{
                 totalCount
@@ -31,12 +40,13 @@ def call_graphQL(url_, api_token):
                 text
                 }
             }
+            hasWikiEnabled
         }
     }'''
 
     json_ = { 'query' : query_ } 
     headers_ = {'Authorization': 'token %s' % api_token}
-    response = requests.post(url=url_, json=json_, headers=headers_)
+    response = requests.post(url=url_graphQL, json=json_, headers=headers_)
     data = json.loads(response.text)
 
     return data
