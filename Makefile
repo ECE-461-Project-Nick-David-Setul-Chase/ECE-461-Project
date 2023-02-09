fc3c86ce987bf627904f4257d563a9c7724c428e
reed369 <reed369@purdue.edu>
2023-02-09 18:37:12 -0500

    working on run.rs

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
+
+# list files to be compiled in comp target
+
+comp:
+	rustc run.rs
+
+run: comp
+	# temporarily just run. run will need commandline arguments
+	./run
+
 delete_everything:
 	rm -R API Brain Metricizer Output ReadWrite Scorer 
--- run.rs
+++ run.rs
@@ -1,20 +1,65 @@
 
 // https://doc.rust-lang.org/book/ch01-02-hello-world.html
 // https://doc.rust-lang.org/std/process/struct.Command.html
+// https://doc.rust-lang.org/book/ch12-02-reading-a-file.html
+// https://doc.rust-lang.org/rust-by-example/std/str.html
+
+//use std::process::Command;
+//use std::env;
+use std::fs;
 
 fn main() {
-    println!("Hello, world!");
 
-    use std::process::Command;
+    let input_file = "run_URL_FILE.txt";
 
-    let input_file = "sample-url-file.txt";
+    println!("In file {}", input_file);
 
-    Command::new("python3")
-            .arg("Metricizer/Metricizer.py")
+    execute_file(input_file);
+}
+
+fn execute_file(input_file: &str) {
+
+    let contents = fs::read_to_string(input_file)
+        .expect("Should have been able to read file");
+
+    let chars: Vec<char> = contents.chars().collect();
+    let mut string = String::new();
+    let mut is_comment = false;
+    let mut has_chars = false;
+
+    for c in chars {
+        if c == '#' {
+            is_comment = true;
+        } else  if c != ' ' && c != '\n' && c != '\t' {
+            has_chars = true;
+        } 
+
+        if c == '\n' {
+            if !is_comment && has_chars {
+                println!("{}", string);
+            }
+
+            string = String::new();
+            is_comment = false;
+            has_chars = false;
+        } else {
+            string.push(c);
+        }
+        //println!("{}", string);
+    }
+
+    //println!("With text:\n{contents}");
+
+    /*Command::new("python3")
+            .arg("Metricizer.py")
             .arg(input_file)
             //.arg("ScorerModule.rs")
             //.arg("Output.rs")
             .spawn()
             .expect("Metricizer failed to start");
-
+*/
 }
+/*
+fn execute_string(string) {
+    
+}*/
