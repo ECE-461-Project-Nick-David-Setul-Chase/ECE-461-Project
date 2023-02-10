7e25ef3d4ad061d70bf52d7248da597f3c076266
NickW09 <60638129+NickW09@users.noreply.github.com>
2023-02-10 18:18:51 -0500

    Update GraphQL.py

--- GraphQL.py
+++ GraphQL.py
@@ -51,3 +51,25 @@ def call_graphQL(url_, api_token):
 
     return data
 
+def filterData(data):
+  
+  readme = (data['data']['repository']['object']['text']) 
+  if not readme:
+    readme_exist = False
+  else: 
+    readme_exist = True
+
+  doc_exist = (data['data']['repository']['hasWikiEnabled'])
+  issues_closed = (data['data']['repository']['issuesClosed']['totalCount'])
+  issues_total = issues_closed + (data['data']['repository']['issuesOpen']['totalCount'])
+  num_contribute = (data['data']['repository']['assignableUsers']['totalCount'])
+  weeks_last_issue = 2
+  license_correct = 1
+  #ISO-8601 encoded UTC date string.
+  
+  print('Has README: ' + str(readme_exist))
+  print('Has good documentation: ' + str(doc_exist))
+  print('Issues closed: ' + str(issues_closed))
+  print('Total issues: ' + str(issues_total))
+  print('Number of assignable users: ' + str(num_contribute))
+
