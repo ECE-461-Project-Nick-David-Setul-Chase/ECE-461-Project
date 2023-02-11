083e0af29c3a01fdebb38e8032dff65f986faf25
NickW09 <nickcreo@yahoo.com>
2023-02-11 18:54:42 -0500

    loading dot

--- Metricizer.py
+++ Metricizer.py
@@ -36,7 +36,7 @@ def metricizer(inputfile):
     #Grab API token
     api_token = os.environ.get("GITHUB_TOKEN") #THE REAL DEAL
     #api_token = "fill in" #FOR TESTING ONLY
-    print(api_token)
+    #print(api_token)
 
     #Creating metric output file
     output_metric = open('output_metric.txt', 'w')
@@ -50,6 +50,8 @@ def metricizer(inputfile):
         
         #path = createDir(url)
         #print(path)
+
+        print(".")
 
         url = url.strip()
         
