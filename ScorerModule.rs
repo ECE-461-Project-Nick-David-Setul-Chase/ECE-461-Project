f6eb40f14849b8e0c400a4552949517305595374
Chase McCoy <chasemccoy@Chases-MacBook-Air.local>
2023-02-11 11:05:00 -0500

    added url errors to scorer

--- ScorerModule.rs
+++ ScorerModule.rs
@@ -56,11 +56,28 @@ fn calculate_scores(full_metrics_strings: Vec<Vec<String>>) -> Vec<[String; 7]> 
         let weeks_since_last_issue: f64 = repo_metrics[6].parse().unwrap();
         let license_correct: f64 = repo_metrics[7].parse().unwrap();
 
-        let ramp_up: f64 = 0.5 * readme_exists + 0.5 * documentation_exists;
-        let correctness: f64 = issues_closed / (issues_total + MIN_POSITIVE); // prevent divide by 0
-        let bus_factor: f64 = 1.0 - (1.0 / num_contributors);
-        let responsive_maintainer: f64 = 1.0 / (weeks_since_last_issue + MIN_POSITIVE); // prevent divide by 0
-        let license: f64 = license_correct;
+        let mut ramp_up: f64 = 0.5 * readme_exists + 0.5 * documentation_exists;
+        let mut correctness: f64 = issues_closed / (issues_total + MIN_POSITIVE); // prevent divide by 0
+        let mut bus_factor: f64 = 1.0 - (1.0 / num_contributors);
+        let mut responsive_maintainer: f64 = 1.0 / (weeks_since_last_issue + MIN_POSITIVE); // prevent divide by 0
+        let mut license: f64 = license_correct;
+
+        if readme_exists == -1.0 { // url error 1
+            ramp_up = -1.0;
+            correctness = -1.0;
+            bus_factor = -1.0;
+            responsive_maintainer = -1.0;
+            license = -1.0;
+        }
+
+        if readme_exists == -2.0 { // url error 2
+            ramp_up = -2.0;
+            correctness = -2.0;
+            bus_factor = -2.0;
+            responsive_maintainer = -2.0;
+            license = -2.0;
+        }
+
         let total: f64 = 0.15 * ramp_up + 
                          0.2 * correctness +
                          0.2 * bus_factor +
